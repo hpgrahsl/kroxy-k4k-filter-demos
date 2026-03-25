e78191764fba6bfcc8292ce9c40f6e7a09e13029
H.P. Grahsl <grahslhp@gmail.com>
2026-03-25 11:00:29 +0100

    fix console consumer params

--- scenario_02/scripts/direct_consumer.sh
+++ scenario_02/scripts/direct_consumer.sh
@@ -8,6 +8,5 @@ TOPIC=demo-kroxy-k4k-jsonsr
 $KAFKA_CLI_TOOLS/kafka-json-schema-console-consumer \
   --bootstrap-server "$BROKER" \
   --property schema.registry.url=http://schema-registry:8081 \
-  --property value.schema="$(cat "$PATH_TO_SCHEMA")" \
   --topic "$TOPIC" \
   --from-beginning
