8542b193bf11a5981eb7612debcd3dd70bdf69f3
H.P. Grahsl <grahslhp@gmail.com>
2026-04-01 13:00:39 +0200

    update scenario 4

--- scenario_04/scripts/proxy_consumer.sh
+++ scenario_04/scripts/proxy_consumer.sh
@@ -4,9 +4,11 @@ KAFKA_CLI_TOOLS=/usr/bin
 BROKER=kroxylicious:9192
 SCHEMA_REGISTRY=http://schema-registry:8081
 TOPIC=demo-kroxy-k4k-payments
+NUM_MESSAGES=1000
 
 $KAFKA_CLI_TOOLS/kafka-avro-console-consumer \
   --bootstrap-server "$BROKER" \
   --property schema.registry.url="$SCHEMA_REGISTRY" \
   --topic "$TOPIC" \
-  --from-beginning
+  --from-beginning \
+  --max-messages $NUM_MESSAGES
